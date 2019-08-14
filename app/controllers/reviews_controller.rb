31a19f8bec926cc7fd71b1e96b68130e3e76f10e
Nicholas Morrissey <n.mozza@hotmail.com>
2019-08-14 14:00:03 +0100

    Yelp Review Complete

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all.where(restaurant_id: params[:restaurant_id])
+  end
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(
+  name: "Nandos",
+  address: "1 Nandos Street",
+  category: "chinese"
+)
+
+Restaurant.create(
+  name: "Pizza Express",
+  address: "1 Pizza Express Street",
+  category: "italian"
+)
+
+Restaurant.create(
+  name: "Yo Sushi",
+  address: "1 Yo Sushi Street",
+  category: "japanese"
+)
+
+Restaurant.create(
+  name: "Belgian Food",
+  address: "1 B Street",
+  category: "belgian"
+)
+
+Restaurant.create(
+  name: "Cafe Rouge",
+  address: "1 Cage Rouge Street",
+  category: "french"
+)
